bed5a96ebfeb8a5d8461b7892c08f6d577ea2c96
Mina Mikhail <mina@fightingtheboss.com>
2017-12-11 12:38:00 -0500

    Add in-class code for 06 Programming Fundamentals Part 1

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -5,16 +5,32 @@
 # (optionally) otherwise
 #   then do something else
 
+count = 9
 
+# if count == 10
+#   puts "Count is equal to ten"
+# else
+#   puts "Count is not equal to ten"
+# end
 
+if count > 10
+  puts "Count is greater than ten"
+elsif count < 10
+  puts "Count is less than ten"
+else
+  puts "Count is equal to ten"
+end
 
+puts "Count is less than ten" if count < 10
 
-# Looping
+unless count > 10
+  "Count is less than or equal to ten"
+end
 
-# for loop
-# while loop
-# until loop
+puts "Count is less than or equal to ten" unless count > 10
 
+discount_rate = ((count > 10) || (count > 100)) ? 0.15 : 0.10
 
+puts "discount_rate is #{discount_rate}"
 
-# Iteration
+puts "End of program"
