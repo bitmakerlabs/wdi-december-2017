400d375d343f19055437193df63974c64cb7dbd7
NatalieBlack <natalievictoriablack@gmail.com>
2017-12-19 11:56:45 -0500

    In-class code for second half of 12-debugging

--- 12-debugging/game.rb
+++ 12-debugging/game.rb
@@ -1,8 +1,17 @@
+require './player.rb'
+require 'pry'
 class Game
 
+
   def initialize(number_of_players)
+    @width = 8
+    @height = 8
+    @players = []
+
     number_of_players.times do
-      @players << Player.new((0..@width).sample, (0..@height).sample)
+      player_x_position = rand(0...@width)
+      player_y_position = rand(0...@height)
+      @players << Player.new(player_x_position, player_y_position)
     end
   end
 
@@ -11,9 +20,8 @@ class Game
     print "How many people are adventuring? "
     number_of_players = gets.chomp.to_i
 
-    g = Game.new(number_of_players)
+    game = Game.new(number_of_players)
     game.turn
-    end
   end
 
   def turn
@@ -24,11 +32,11 @@ class Game
       new_x = moves.sample
       new_y = moves.sample
 
-      if new_x >= 0 && new_x < width
+      if new_x >= 0 && new_x < @width
         player.x += new_x
       end
 
-      if new_y >= 0 && new_y < height
+      if new_y >= 0 && new_y < @height
         player.y += new_y
       end
     end
