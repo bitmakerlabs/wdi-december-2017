df7f88abde4b255496598de17919a1a7a9c4675a
Fred Ngo <bitmakerlabs@Bitmaker-7.local>
2018-01-18 11:45:35 -0500

    [28-sessions-and-authentication] Generate Sessions controller with: bin/rails generate controller Sessions new create destroy --no-test-framework --no-helper

--- 28-sessions-and-authentication/instructor/findr/config/routes.rb
+++ 28-sessions-and-authentication/instructor/findr/config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
 
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :users, only: %i(new create)
