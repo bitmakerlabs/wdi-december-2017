8a5f8131064918733517b94566b2194011b3d3f6
Fred Ngo <bitmakerlabs@Bitmaker-7.local>
2018-01-18 11:42:28 -0500

    [28-sessions-and-authentication] Implement UsersController#create, display error messages on fields with errors

--- 28-sessions-and-authentication/instructor/findr/app/controllers/users_controller.rb
+++ 28-sessions-and-authentication/instructor/findr/app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new
+
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+
+    if @user.save
+      # User _url in controllers, use _path in views
+      redirect_to root_url
+    else
+      render :new
+    end
   end
+
+
 end
