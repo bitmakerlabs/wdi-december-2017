96cd5377f0d07cd80be7959ed4ff238f2a34c0e9
Fred Ngo <bitmakerlabs@Bitmaker-7.local>
2018-01-18 11:48:15 -0500

    [28-sessions-and-authentication] Remove useless session views, change session routes to RESTful

--- 28-sessions-and-authentication/instructor/findr/config/routes.rb
+++ 28-sessions-and-authentication/instructor/findr/config/routes.rb
@@ -1,16 +1,9 @@
 Rails.application.routes.draw do
 
-
-  get 'sessions/new'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :users, only: %i(new create)
-
+  resource :sessions, only: %i(new create destroy)
   resources :profiles, only: :index
 
   root 'profiles#index'
