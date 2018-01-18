08e661fbd6dac18a669265d141b6cd8a709d3dbe
Fred Ngo <bitmakerlabs@Bitmaker-7.local>
2018-01-18 11:23:43 -0500

    [28-sessions-and-authentication] Remote useless create view, update routes to be restful

--- 28-sessions-and-authentication/instructor/findr/config/routes.rb
+++ 28-sessions-and-authentication/instructor/findr/config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/create'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :profiles
+  resource :users, only: %i(new create)
+
+  resources :profiles, only: :index
 
   root 'profiles#index'
 end
