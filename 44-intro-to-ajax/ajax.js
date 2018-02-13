295a43847f2fed4c5cfa69cde30148e9743a92e8
Jesse Mykolyn <jrmykolyn@gmail.com>
2018-02-13 13:19:52 -0500

    [44 - Intro to AJAX] Add in-class code.

--- 44-intro-to-ajax/ajax.js
+++ 44-intro-to-ajax/ajax.js
@@ -1,3 +1,61 @@
-$(function() {
+window.addEventListener( 'DOMContentLoaded', function() {
 
-});
+    // --------------------------------------------------
+    // GET MONSTERS
+    // --------------------------------------------------
+    var monsterButton = document.querySelector( '#get_monsters' );
+
+    monsterButton.addEventListener( 'click', function( e ) {
+        console.log( 'BEFORE AJAX REQUEST' );
+
+        // Create/dispatch request, capture placeholder in `request` var.
+        var request = $.ajax( {
+            url: 'https://monsters-api.herokuapp.com/monsters',
+            method: 'GET',
+            dataType: 'html'
+        } );
+
+        // Provide callback to handle case where request succeeds.
+        request.done( function( responseData ) {
+            document.body.innerHTML += responseData;
+        } );
+
+        // Provide callback to handle case where request fails.
+        request.fail( function( jqXHR, textStatus, errorThrown ) {
+            var errorMsg = document.createElement( 'p' );
+            errorMsg.innerHTML = 'Whoops, something went wrong! Try again later.';
+            document.body.appendChild( errorMsg );
+        } );
+    } );
+
+
+    // --------------------------------------------------
+    // POST NEW MONSTER DATA
+    // --------------------------------------------------
+    var postMonsterButton = document.querySelector( '#post_new_monster' );
+
+    postMonsterButton.addEventListener( 'click', function() {
+
+        // Create/dispatch request, chain `.done()` and `.fail()` method calls to return values.
+        jQuery.ajax( {
+            url: 'https://monsters-api.herokuapp.com/monsters',
+            method: 'POST',
+            data: {
+                monster: {
+                    name: 'The Deep Sea King',
+                    home: 'City Z',
+                    creepiness: 100
+                }
+            }
+        } ).done( function( responseData ) {
+
+            console.log( 'SUCCESSFULLY POSTEDE MONSTER DATA!' );
+            console.log( responseData );
+
+        } ).fail( function( jqXHR, textStatus, errorThrown ) {
+
+            console.log( 'FAILED TO POST MONSTER DATA!' );
+
+        } );
+    } );
+} );
